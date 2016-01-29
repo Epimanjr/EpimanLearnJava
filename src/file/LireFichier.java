3ce96290922d840fac4d1f58b4c8a3ee2101e860
TheMaster57 <maxime.blaise12@gmail.com>
2016-01-29 22:18:12 +0100

    Lecture d'un fichier

--- src/file/LireFichier.java
+++ src/file/LireFichier.java
@@ -15,15 +15,44 @@ public class LireFichier {
 
     /**
      * Méthode principale.
-     * Démonstration: Lecture d'un fichier !
      * 
      * @param args Arguments.
      */
     public static void main(String[] args) {
         // Affiche le contenu de notre fichier FileForRead.txt
         afficherLeContenu("file/FileForRead.txt");
+        // Stockage du contenu de notre fichier
+        String contenuDuFichier = stockerLeContenu("file/FileForRead.txt");
+        //System.out.println(contenuDuFichier);
     }
 
+    /**
+     * Lit et stocke en mémoire le contenu d'un fichier.
+     *
+     * @param cheminVersFichier Le chemin vers le fichier.
+     */
+    public static String stockerLeContenu(String cheminVersFichier) {
+        try {
+            // Ouverture d'un Buffer de lecture
+            BufferedReader br = new BufferedReader(new FileReader(cheminVersFichier));
+            // On parcourt chaque ligne, et on mémorise !
+            StringBuffer memoire = new StringBuffer();
+            while(br.ready()) {
+                // Ajout de la ligne en mémoire
+                memoire.append(br.readLine()+"\n");
+            }
+            // Fermeture du Buffer
+            br.close();
+            // Notre mémoire est complète, le fichier est fini d'être lu
+            return memoire.toString();
+        } catch(FileNotFoundException file) {
+            System.err.println("Erreur: le fichier n'a pas été trouvé.");
+        } catch(IOException io) {
+            System.err.println("Erreur: java n'arrive pas à lire le fichier.");
+        }
+        return "";
+    }
+    
     /**
      * Affiche le contenu d'un fichier.
      *
